8eb1106d8bd27dd575780c71537fdaf5cfd6d84b
Ivan Bessarabov <ivan@bessarabov.ru>
2020-02-18 13:54:52 +0300

    Ouput list of mp3 files from rss

--- code/main.go
+++ code/main.go
@@ -1,7 +1,56 @@
 package main
 
-import "fmt"
+import (
+	"encoding/xml"
+	"fmt"
+	"net/http"
+	"os"
+)
+
+type Channel struct {
+	Items []Item `xml:"channel>item"`
+}
+
+type Enclosure struct {
+	Url    string `xml:"url,attr"`
+	Length int64  `xml:"length,attr"`
+	Type   string `xml:"type,attr"`
+}
+
+type Item struct {
+	Description string    `xml:"description"`
+	Enclosure   Enclosure `xml:"enclosure"`
+	Link        string    `xml:"link"`
+	Title       string    `xml:"title"`
+}
 
 func main() {
-    fmt.Println("test")
+
+	rss_url := "https://radio-t.com/podcast.rss"
+
+	resp, err := http.Get(rss_url)
+
+	if err != nil {
+		fmt.Printf("%s", err)
+		os.Exit(1)
+	}
+
+	defer resp.Body.Close()
+
+	channel := Channel{}
+
+	if err := xml.NewDecoder(resp.Body).Decode(&channel); err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+
+	if len(channel.Items) == 0 {
+		fmt.Println("No items")
+		os.Exit(1)
+	}
+
+	for i, item := range channel.Items {
+		fmt.Printf("%v. item title: %v\n", i, item.Enclosure.Url)
+	}
+
 }
