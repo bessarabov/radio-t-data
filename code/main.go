4bf589df299a93c42948004bff38a58507ec83c9
Ivan Bessarabov <ivan@bessarabov.ru>
2020-02-18 15:43:38 +0300

    Saving file md5

--- code/main.go
+++ code/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"crypto/md5"
 	"encoding/json"
 	"encoding/xml"
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 	"regexp"
@@ -27,6 +29,30 @@ type Item struct {
 	Title       string    `xml:"title"`
 }
 
+func save_file(url string, file_name string) int64 {
+
+	out, err := os.Create(file_name)
+	defer out.Close()
+
+	if err != nil {
+		os.Exit(1)
+	}
+
+	resp, err := http.Get(url)
+	if err != nil {
+		os.Exit(1)
+	}
+
+	defer resp.Body.Close()
+
+	n, err := io.Copy(out, resp.Body)
+	if err != nil {
+		os.Exit(1)
+	}
+
+	return n
+}
+
 func parse_mp3(url string) string {
 
 	fmt.Println(url)
@@ -35,24 +61,30 @@ func parse_mp3(url string) string {
 
 	episode_number := re.FindStringSubmatch(url)[1]
 
+	num, e := strconv.ParseInt(episode_number, 10, 64)
+	if e != nil {
+		os.Exit(1)
+	}
+
+	tmp_file_name := "/tmp/" + episode_number + ".mp3"
+
+	save_file(url, tmp_file_name)
+
 	type RadioTFile struct {
 		Number int64  `json:"number"`
 		Url    string `json:"url"`
+		Md5    string `json:"md5"`
 	}
 
 	type RadioTEpisode struct {
 		File RadioTFile `json:"file"`
 	}
 
-	num, e := strconv.ParseInt(episode_number, 10, 64)
-	if e != nil {
-		os.Exit(1)
-	}
-
 	episode := RadioTEpisode{
 		File: RadioTFile{
 			Number: num,
 			Url:    url,
+			Md5:    get_md5_from_file(tmp_file_name),
 		},
 	}
 
@@ -76,11 +108,29 @@ func parse_mp3(url string) string {
 	return "ok"
 }
 
+func get_md5_from_file(file_name string) string {
+	f, err := os.Open(file_name)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer f.Close()
+
+	h := md5.New()
+	if _, err := io.Copy(h, f); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	return fmt.Sprintf("%x", h.Sum(nil))
+}
+
 func main() {
 
-	rss_url := "https://radio-t.com/podcast.rss"
+	// tmp
+	os.Exit(1)
 
-	parse_mp3("http://cdn.radio-t.com/rt_podcast672.mp3")
+	rss_url := "https://radio-t.com/podcast.rss"
 
 	resp, err := http.Get(rss_url)
 
